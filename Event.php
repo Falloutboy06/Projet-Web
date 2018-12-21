d9b645c0ef94f4495b7f5d210d3ece64d3195eb3
Unknown <guillaume.subrini@gmail.com>
2018-12-21 10:35:08 +0100

    Affichage base de données ( finir : creation event + css affichage)

--- Event.php
+++ Event.php
@@ -1,5 +1,14 @@
 <html>
-    <?php session_start();?>
+    <?php session_start();
+        try {
+            $bdd= new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8','root','');
+        }
+        catch(Exception $e) {
+            die('Erreur:'.$e->getMessage());
+        }  
+        $SQL ="SELECT event.ID_Festival as IDF,event.Titre as Titre,user.name as createur, event.Festival as Fest,event.DateDebut as DD, event.DateFin as DF,event.Info as Info FROM event INNER JOIN user ON ID_Crea = user.userID";
+        $reponse=$bdd->query("$SQL");
+        ?>
         <head>
             <title>Event Metal Fest</title>
             <link rel="stylesheet" href="Event.css">
@@ -15,29 +24,27 @@
                     <?php if($_SESSION['LoggedIn']==1){include('./DecoButton.php');}?>
                 </tr>
             </table>
-            <img id="image1" src="./bande_noir.jpeg">
-            <img id="image2" src="./bande_noir.jpeg">
+            <img class="image1" src="./bande_noir.jpeg">
+            <img class="image2" src="./bande_noir.jpeg">
             
-            <?php
-                echo $_SESSION['userID'];
-            ?>
-
-            <div id="event"
-                <?php
+            <div id="event">
+                    <?php
                     foreach($reponse as $row)
                     {
-                ?>
-                    <p>
-                        <h2> Evenements </h2> <?php echo $row['Titre'] ?></br>
-                        <h2> Festival </h2> <?php echo $row['Festival'] ?></br>
-                        <h2> Date de Debut </h2> <?php echo $row['DateDebut'] ?></br>
-                        <h2> Date de Fin </h2> <?php echo $row['DateFin'] ?></br>
-                        <h2> Information </h2> <?php echo $row['Info'] ?></br>
-                    </p>
-                <?php
-                }
-                $reponse->closeCursor();
-                ?>
+                        ?>
+                        <p>
+                            <h2> ------------------------------------------------------------------------------ </h2></br>
+                            <h2> Evenements </h2> <?php  echo $row['Titre'] ?></br>
+                            <h2> Createur </h2> <?php echo $row['createur'] ?></br>
+                            <h2> Festival </h2> <?php echo $row['Fest'] ?></br>
+                            <h2> Date de Debut </h2> <?php  echo $row['DD'] ?></br>
+                            <h2> Date de Fin </h2> <?php  echo $row['DF'] ?></br>
+                            <h2> Informations </h2> <?php  echo $row['Info'] ?></br>
+                        </p>
+                    <?php
+                    }
+                    $reponse->closeCursor();
+                    ?>
             </div>
         </body>
 </html>
